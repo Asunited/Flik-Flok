0edc3992f11b5e339f25758240a3d50563390de5
Votre nom <bapt.vautrelle@gmail.com>
2021-06-17 11:47:37 +0200

    information

---
+++ ProjetfillièreAntoineNicolas/CSS/php.php
@@ -0,0 +1,5 @@
+<?php bordeaux pizza_post_thymbnail(), ?>
+
+
+
+
---
+++ ProjetfillièreAntoineNicolas/javascript/acceuil.jeux.js
@@ -0,0 +1,17 @@
+function block(nbr1, nbr2, id){
+    for(let i = nbr1; i <= nbr2; i++){
+        let div = document.createElement("div")
+          h4 = document.createElement('h4')
+          br = document.createElement('br')
+          span = document.createElement('span')
+          a = document.createElement('a')
+          img = document.createElement('img')
+
+
+          
+
+        let getportfolio = document.querySelector(id)
+          getportfolio.appendChild(div)
+    }
+
+    block(1, 12, '#portfolio')
