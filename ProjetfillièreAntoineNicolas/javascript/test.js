9afca672e290af99242d1e936df1b9c1a5989d8d
Votre nom <bapt.vautrelle@gmail.com>
2021-06-29 17:38:02 +0200

    message

---
+++ ProjetfillièreAntoineNicolas/javascript/test.js
@@ -0,0 +1,11 @@
+<script>
+        function imagefun(){   
+            var Image_Id = document.getElementById('getImage');
+            if (Image_Id.src.match("imageName1.jpg")) {
+                Image_Id.src = "imageName2.jpg";
+            }
+            else {
+                Image_Id.src = "imageName1.jpg";
+            }
+        }        
+    </script>
