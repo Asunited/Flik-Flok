25fdc4d0a3b143d07858000418b33e9d7a9d5bf9
Votre nom <bapt.vautrelle@gmail.com>
2021-07-22 09:37:45 +0200

    message

--- ProjetfillièreAntoineNicolas/javascript/score.js
+++ ProjetfillièreAntoineNicolas/javascript/score.js
@@ -5,9 +5,23 @@ console.log(child.fakeID)
 
 let parent = localStorage.getItem('Parent')
 parent= JSON.parse(parent)
-console.log(parent.id)
 let cal = document.getElementById('calcul')
-if(child.fakeID == parent.id) {
+if(parent){
+    if(child.fakeID == parent.id) {
+        let score = localStorage.getItem('score')
+        score = JSON.parse(score)
+        total1.innerHTML=`${score.total1}`
+        total2.innerHTML=`${score.total2}`
+        total3.innerHTML=`${score.total3}`
+        total4.innerHTML=`${score.total4}`
+    
+    
+        let sT = parseInt(score.total1) + parseInt(score.total2) + parseInt(score.total3) + parseInt(score.total4)
+        cal.innerHTML = `${sT}`
+        } else {
+            cal.innerHTML = 'Enfant pas reconnu'
+    }
+}else {
     let score = localStorage.getItem('score')
     score = JSON.parse(score)
     total1.innerHTML=`${score.total1}`
@@ -18,8 +32,6 @@ if(child.fakeID == parent.id) {
 
     let sT = parseInt(score.total1) + parseInt(score.total2) + parseInt(score.total3) + parseInt(score.total4)
     cal.innerHTML = `${sT}`
-    } else {
-        cal.innerHTML = 'Enfant pas reconnu'
-}
+    }
 
 
